513a8f8279fb9d89425196c0ae13940b33f0444f
asperata <1371513516@qq.com>
2020-02-26 15:41:44 +0800

    555

--- src/cn/dl/wc/no1/Test1.java
+++ src/cn/dl/wc/no1/Test1.java
@@ -1,10 +1,16 @@
 package cn.dl.wc.no1;
-
+/*
+ * 1、客户端必须先更新(pull)再提交（commit and push）
+ * 2、再提交的时候，不要全项目一起提交，写了哪个文件，就只提交哪个文件；
+ * 3.
+ * 
+ * 
+ */
 public class Test1 {
 	public void show() {
 		System.out.println("1111");
 		System.out.println("你好");
 		System.out.println("world");
-		
+		System.out.println("22222222");
 	}
 }
